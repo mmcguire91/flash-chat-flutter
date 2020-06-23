60c07a64a0e39c8e26e78995a987c4aac529f724
mmcguire91 <mmcguire91@gmail.com>
2020-06-23 17:08:24 -0500

    Completed Navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: LoginScreen.id,
+      initialRoute: WelcomeScreen.id,
       routes: {
         LoginScreen.id: (context) => LoginScreen(),
         WelcomeScreen.id: (context) => WelcomeScreen(),
